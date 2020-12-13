b8a1c5d8caf6adcb0ca3a32cda965fc094c7f6fc
ndgndg91 <ndgndg91@gmail.com>
2020-12-13 22:37:30 +0900

    <fix> IndexInterceptor

--- src/main/java/com/something/my/configuration/WebConfiguration.java
+++ src/main/java/com/something/my/configuration/WebConfiguration.java
@@ -18,7 +18,7 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new IndexInterceptor()).addPathPatterns("/");
+        registry.addInterceptor(new IndexInterceptor()).addPathPatterns("").addPathPatterns("/");
         registry.addInterceptor(new AuthInterceptor()).addPathPatterns("/u/**").addPathPatterns("/apis/**");
         registry.addInterceptor(new TrialRouteInterceptor()).addPathPatterns(TRIAL_ROUTES);
     }
