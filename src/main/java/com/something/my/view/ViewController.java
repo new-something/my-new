f93fa3f42aa5878ba0c9ece62b8c5f83725d96da
ndgndg91 <ndgndg91@gmail.com>
2020-12-16 21:10:28 +0900

    <feat> logout

--- src/main/java/com/something/my/view/ViewController.java
+++ src/main/java/com/something/my/view/ViewController.java
@@ -2,6 +2,9 @@ package com.something.my.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ViewController {
@@ -9,5 +12,13 @@ public class ViewController {
     @GetMapping("/u/dashboard")
     public String dashboard(){
         return "dashboard";
+    }
+
+    @GetMapping("/s/logout")
+    public RedirectView logout(
+            HttpSession session
+    ){
+        session.invalidate();
+        return new RedirectView("/");
     }
 }
